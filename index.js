ac823b6ed8f7ed18f65f5eb75a1712ec46a3246c
pkamanthe <patriciamumbua97@gmail.com>
2024-10-18 11:47:02 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((resp) => resp.json())
+    .then((data) => renderBooks(data))
+    .catch((error) => console.error("Error fetching books:", error));
 }
-
 function renderBooks(books) {
-  const main = document.querySelector('main');
+  const booksList = document.getElementById("books-list");
+  booksList.innerHTML = ""; 
+
   books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
+    const listItem = document.createElement("li");
+    listItem.textContent = book.name; 
+    booksList.appendChild(listItem);
   });
 }
-
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", () => {
   fetchBooks();
 });
